cd7eee7b49210061996d65dc6623e210db84750a
Minisammich <91817698+Minisammich@users.noreply.github.com>
2024-03-05 14:58:36 -0600

    Finished Interactive Scene

--- InteractiveScene/sketch.js
+++ InteractiveScene/sketch.js
@@ -1,16 +1,22 @@
+let canvasX;
+let canvasY;
+
 function setup() {
-  createCanvas(800, 500);
+  canvasX = windowWidth;
+  canvasY = windowHeight;
+  createCanvas(canvasX, canvasY);
   noStroke();
-  let mPosLastLClick;
-  let mPosLastMClick;
 }
 
-mPosLastLClick = 800/9+10;
-mPosLastMClick = 800/9+10;
+let mPosLastLClick = 800/8;
+let mPosLastMClick = 800/8;
+let moonCoverOffset = -15;
+let rotationOffset;
 
-function inMoonBounds(){
-  if(mouseX>=mPosLastMClick-60&&mouseX<=mPosLastMClick+50&&
-     mouseY>=height/9-30&&mouseY<=height/9+30){
+function inMoonBounds(){ //checks if cursor is within the bounds of the moon
+  let moonScale = ((canvasX+canvasY)/2)/10; //sets scale of the moon based on canvas sign
+  if(mouseX>=mPosLastMClick-(moonScale+10)&&mouseX<=mPosLastMClick+(moonScale+10)&&
+     mouseY>=height/9-(moonScale-10)&&mouseY<=height/9+(moonScale-10)){
     return(true);
   } else {
     return(false);
@@ -18,13 +24,18 @@ function inMoonBounds(){
 }
 
 function lastClickedPos(){
-  if(mouseIsPressed&&mouseButton===LEFT&&inMoonBounds()===true) { //Grabs mouseX position while Left Click is held.
+
+  //Grabs and stores mouseX position while Left Click is held.
+  if(mouseIsPressed&&mouseButton===LEFT&&inMoonBounds()===true) {
     mPosLastLClick = mouseX;
+    moonCoverOffset = mPosLastMClick-mPosLastLClick;
   }
+
+  //Grabs and stores mouseX position while Middle Click is held.
   if(mouseIsPressed&&mouseButton===CENTER&&inMoonBounds()===true) {
     mPosLastMClick = mouseX;
-    mPosLastLClick -= (mPosLastLClick-mPosLastMClick);
   }
+
 }
 
 function grass(light) { //Fills in bottom section of the screen with grass, dimly lit by the moon.
@@ -35,8 +46,9 @@ function grass(light) { //Fills in bottom section of the screen with grass, diml
 }
 
 function mountains(light) {
-  let fgLight=(light/2)
-  let rotationOffset = 5;
+  let fgLight=(light/2) //makes FG affected less than BG mountains by light
+
+  //Background Mountains
   fill(light+19-rotationOffset, light+14-rotationOffset, light+27-rotationOffset);
   triangle(-100, height/1.55, width/3, height/1.55, width/6.5, height/6);
   fill(light+15-(rotationOffset/2), light+10-(rotationOffset/2), light+25-(rotationOffset/2));
@@ -46,6 +58,7 @@ function mountains(light) {
   fill(light+10+rotationOffset,light+7+rotationOffset,light+19+rotationOffset);
   triangle(width/1.7, height/1.55, width*1.2, height/1.55, width/1.2, height/7);
   
+  //Foreground Mountains
   fill(45+fgLight-rotationOffset,50+fgLight-rotationOffset,45+fgLight-rotationOffset);
   triangle(-50, height/1.55, width/4, height/1.55, width/6, height/3.3);
   fill(43+fgLight-(rotationOffset/2),47+fgLight-(rotationOffset/2),43+fgLight-(rotationOffset/2));
@@ -56,32 +69,31 @@ function mountains(light) {
   triangle(width/2, height/1.55, width*1.25, height/1.55, width/1.2, height/3);
 }
 
-function moon(mPosLC,mPosMC) {
+function moon(){
+  let moonScale = ((canvasX+canvasY)/2)/10; //sets scale of the moon based on canvas sign
   let light;
-  let currentLight;
-  let moonCurrentPos=mPosMC+(mPosLC-mPosMC);
-  let moonLightPos;
-  fill(175,175,125);
-  circle(mPosMC-10, height/9, 50);
-   //Checks if mouse is clicked and on the moon
-  if(mPosLC>width/9) { // if/elseif for determining light level based on moon's position
-    currentLight=(mPosLC/10)+3;
-  } else if(mPosLC<width/9) {
-    currentLight=((width/9-mPosLC)/6)+10;
+  let moonPos = mPosLastMClick;
+  let moonCoverPos = mPosLastMClick-moonCoverOffset; //sets position of the shadow on the moon
+  if(moonCoverOffset>=0){ //Sets light value based on the offset of the shadow relative to the moon
+    light = ((moonCoverOffset/3)/(moonScale/50));
+  } else {
+    light = -((moonCoverOffset/3)/(moonScale/50));
   }
   
-  light=currentLight;
-  
-  fill(30,25,100);
-  circle((moonCurrentPos), height/9, 45);
-  return((light-7));
-  
+  fill(175,175,125); //fills in the moons colour
+  circle(moonPos,height/9, moonScale);
+
+  fill(30,25,100); //fills in the shadow on the moon as BG colour
+  circle(moonCoverPos, height/9, moonScale-3);
+
+  rotationOffset = (moonPos/50); //sets offset of the light based on position of the moon
+  return(light);
 }
 
 function draw() {
   background(30,25,100); //Nice dark blue night sky.
   lastClickedPos();
-  let moonLight = moon(mPosLastLClick,mPosLastMClick); //draws moon and assigns output to variable.
+  let moonLight = moon(); //draws moon and assigns output to variable.
   grass(moonLight);     //draws grass with the R and G values increasing with moonLight.
   mountains(moonLight); //draws mountains with the RGB values increasing with moonLight.
 }
