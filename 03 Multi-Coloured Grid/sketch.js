b23c448f9334cf74e7a46dad60fec7b299c8b0be
Minisammich <91817698+Minisammich@users.noreply.github.com>
2024-03-07 15:20:07 -0600

    Made Grid Work

--- 03 Multi-Coloured Grid/sketch.js
+++ 03 Multi-Coloured Grid/sketch.js
@@ -5,34 +5,54 @@
 // Extra for Experts:
 // - Creates a grid of random colours, takes inputs to change size and randomize colours.
 
-let squareSize=10, columns, rows;
+let squareSize=40,columns,rows,xPos,yPos,colourList,rX,rY,squareColour;
 
 function setup() {
+  document.addEventListener("contextmenu", event => event.preventDefault())
   createCanvas(windowWidth, windowHeight);
   columns = windowWidth/squareSize;
-  rows = columns = windowHeight/squareSize;
+  rows = windowHeight/squareSize;
+  calculateGrid();
 }
 
 function mousePressed() {
   if(mouseButton===LEFT) {
-    squareSize--;
+    squareSize/=1.2;
   } else if(mouseButton===RIGHT) {
-    squareSize++;
+    squareSize*=1.2;
   }
 
   columns = windowWidth/squareSize;
-  rows = columns = windowHeight/squareSize;
+  rows = windowHeight/squareSize;
+
+  calculateGrid();
+  console.log(colourList)
 }
 
-function grid() {
-  for(i=0; i<=rows; i++) {
-    for(i=0; i<=columns; i++) {
-      //buh
+function calculateGrid() {
+  xPos = []; yPos = []; colourList = [];
+  for(let yCount=0; yCount<rows; yCount++) {
+    for(let xCount=0; xCount<columns; xCount++) {
+      if(xCount*squareSize<width&&yCount*squareSize<height){
+        xPos.push(xCount*squareSize);
+        yPos.push(yCount*squareSize);
+        colourList.push([random(0,255),random(0,255),random(0,255)]);
+      }
     }
+    
   }
-  
+}
+
+function drawGrid() { 
+  for(let i=0; i < xPos.length; i++) {
+    rX = xPos[i]; rY = yPos[i]; squareColour = colourList[i];
+    fill(squareColour);
+    rect(rX, rY, rX+squareSize, rY+squareSize);
+  }
 }
 
 function draw() {
+  frameRate(60);
   background(220);
+  drawGrid();
 }
