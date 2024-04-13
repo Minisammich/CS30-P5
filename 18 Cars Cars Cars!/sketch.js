b59454e83cf34ec4d7173b9c6c9b7d15a9de9c57
Minisammich <91817698+Minisammich@users.noreply.github.com>
2024-04-12 22:00:43 -0600

    car

--- 18 Cars Cars Cars!/sketch.js
+++ 18 Cars Cars Cars!/sketch.js
@@ -9,14 +9,20 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CORNERS);
+  noStroke();
 }
 
 function draw() {
   background(90);
-  fill(0);
+  fill(25);
+
+  road();
+}
+
+function road() {
   rect(0,height*0.2,width,height*0.8);
-  for(i = 0; i < width; i += 100) {
+  for(i = 0; i < width; i += 150) {
     fill(200,200,50);
-    rect(i,(height/2 + 5), i+50, (height/2 - 5));
+    rect(i,(height/2 + 5), i+70, (height/2 - 5));
   }
 }
