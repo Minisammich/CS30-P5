f6ef2c92d16e37ca98d5dd7dd7d3cd062c04c371
Minisammich <91817698+Minisammich@users.noreply.github.com>
2024-04-25 13:05:34 -0600

    Done I think?

--- 20 Puzzle Game Starter/sketch.js
+++ 20 Puzzle Game Starter/sketch.js
@@ -23,13 +23,10 @@ let row, col;
 
 function setup() {
   createCanvas(NUM_COLS * squareSize, NUM_ROWS * squareSize);
+  textSize(50);
+  textAlign(CENTER);
 
-  for(let i = 0; i < NUM_ROWS; i++) {
-    grid.push([]);
-    for(let j = 0; j < NUM_COLS; j++) {
-      grid[i][j] = round(random(1)); 
-    }
-  }
+  randomizeGrid();
 }
 
 function draw() {
@@ -37,21 +34,63 @@ function draw() {
   col = getCurrentX();
   background(220);
   drawGrid();
+  drawSelected(col,row);
+  if(winState) drawWinText();
 }
 
 function mousePressed() { // Flip grid squares when mouse is pressed
-  if(mouseX < width && mouseY < height && mouseButton === LEFT){
-    flip(col,row); // Flip current square
+  if(mouseX < width && mouseY < height && mouseButton === LEFT && winState === false){
+    if(keyIsPressed && keyCode === SHIFT) {
+      flip(col,row); // Flip current square
+    } else if(winState === false) {
+      flip(col,row); // Flip current square
 
-    if(col != NUM_COLS-1) flip(col+1,row); // Flip square to the RIGHT.
+      if(col != NUM_COLS-1) flip(col+1,row); // Flip square to the RIGHT.
 
-    if(col != 0) flip(col-1,row); // Flip square to the LEFT.
+      if(col != 0) flip(col-1,row); // Flip square to the LEFT.
 
-    if(row != NUM_ROWS-1) flip(col,row+1); // Flip square ABOVE.
+      if(row != NUM_ROWS-1) flip(col,row+1); // Flip square ABOVE.
 
-    if(row != 0) flip(col,row-1); // Flip square BELOW.
+      if(row != 0) flip(col,row-1); // Flip square BELOW.
+    }
 
   }
+}
+
+function keyPressed() {
+  if(keyCode === 32 && winState) {
+    randomizeGrid();
+    winState = false;
+  }
+}
+
+function drawSelected(col,row) {
+  fill(0,255,0,100);
+  if(mouseX < width && mouseY < height) {
+    if(keyIsPressed && keyCode === SHIFT && winState === false){
+      rect(col*squareSize,row*squareSize,squareSize);
+    } else if(winState === false) {
+      rect(col*squareSize,row*squareSize,squareSize); // Over current square
+
+        if(col != NUM_COLS-1) rect((col+1)*squareSize,row*squareSize,squareSize); // Over square to the RIGHT.
+
+        if(col != 0) rect((col-1)*squareSize,row*squareSize,squareSize); // Over square to the LEFT.
+
+        if(row != NUM_ROWS-1) rect(col*squareSize,(row+1)*squareSize,squareSize); // Over square ABOVE.
+
+        if(row != 0) rect(col*squareSize,(row-1)*squareSize,squareSize); // Over square BELOW.
+    }
+  }
+}
+
+function drawWinText() {
+  fill(0,255,0);
+  textSize(((NUM_COLS+NUM_ROWS)*squareSize)/10); 
+  text("You Win!",width*0.5,height*0.5);
+
+  fill(0,255,255);
+  textSize(((NUM_COLS+NUM_ROWS)*squareSize)/30);
+  text("Press SPACE to Reset",width*0.5,height*0.8);
 }
 
 function flip(x,y) {
@@ -72,6 +111,15 @@ function getCurrentY() { // Determines current column of the mouse.
   return(currRow);
 }
 
+function randomizeGrid() {
+  for(let i = 0; i < NUM_ROWS; i++) {
+    grid.push([]);
+    for(let j = 0; j < NUM_COLS; j++) {
+      grid[i][j] = round(random(1)); 
+    }
+  }
+}
+
 function drawGrid() {
   let maxSquare = 0, minSquare = 255;
   for(let y = 0; y < NUM_ROWS; y++) {
@@ -88,7 +136,7 @@ function drawGrid() {
 
       if(minSquare === maxSquare) {
         winState = true;
-      }
+      } else { winState = false; }
     }
   }
 
