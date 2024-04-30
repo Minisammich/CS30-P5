274511d4ddf9ec6f74659b5021361df80ee2ff38
Minisammich <91817698+Minisammich@users.noreply.github.com>
2024-04-30 12:01:00 -0600

    The physics are actually physicing now!
     MAJOR FLAWS:
    -Array order affects function.
    -No X collisions or movement, only Y.
    -Only bottom edge checked for collisions.
    -Objects can be wedged together, explode outward when the object singularity is lifted then dropped.
    -Bounce on collision with another object often cancelled.

--- 17 Physics/sketch.js
+++ 17 Physics/sketch.js
@@ -5,6 +5,20 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
+
+// MAJOR FLAWS:
+//
+// Array order affects function.
+//
+// No X collisions or movement, only Y.
+//
+// Only bottom edge checked for collisions.
+//
+// Objects can be wedged together, explode outward when the object singularity is lifted then dropped.
+//
+// Bounce on collision with another object often cancelled.
+
 let physicsObjects = [];
 let shapeState = "rect";
 
@@ -18,6 +32,8 @@ function setup() {
   document.addEventListener("contextmenu", event => event.preventDefault())
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
+  angleMode(DEGREES);
+  textAlign(CENTER);
 }
 
 function draw() {
@@ -38,30 +54,16 @@ function draw() {
   
   // Loop through the objects array.
   for(i = 0; i < physicsObjects.length; i++) {
-    let s1 = physicsObjects[i];
-    let colliding = false;
+    let obj1 = physicsObjects[i];
 
-    s1.display();
-    let s1X = s1.getPos[0] + s1.size, s1Y = s1.getPos[1] + s1.size;
-
-    for(j = 0; j < physicsObjects.length; j++) {
-      if(i !== j){
-        let s2 = physicsObjects[j];
-        if(s2.isInBounds(s1X,s1Y)) {
-          colliding = true;
-        }
-      }
-    }
-    
-    if(colliding) {
-      s1.xSpeed *= -1;
-    }
+    obj1.display();
 
     // If object is picked up, move it with cursor, else use gravity.
-    if(s1.isPickedUp()) {
-      s1.move();
+    if(obj1.isPickedUp()) {
+      obj1.move();
     } else {
-      s1.gravity();
+      let landingPos = obj1.collisions(i);
+      obj1.gravity(landingPos);
     }
   }
 }
@@ -73,10 +75,16 @@ function launchArrow(x,y) {
   if(yOffset > y+10 || yOffset < y-10) {
     if(yOffset < y) {
       line(x,y,xOffset,yOffset);
-      triangle(xOffset,yOffset,xOffset-5,yOffset+10,xOffset+5,yOffset+10);
+      push();
+      translate(xOffset,yOffset);
+      triangle(0,0,-5,10,5,10);
+      pop();
     } else {
       line(x,y,xOffset,yOffset);
-      triangle(xOffset,yOffset,xOffset-5,yOffset-10,xOffset+5,yOffset-10);
+      push();
+      translate(xOffset,yOffset);
+      triangle(0,0,-5,-10,5,-10);
+      pop();
     }
   }
 }
@@ -139,8 +147,6 @@ function mouseWheel() {
   } else if(launchModifierState === x) {
     launchXSpeed += mWheel;
   }
-
-  console.log(launchYSpeed,launchXSpeed);
 }
 
 class PhysicsObject {
@@ -156,7 +162,7 @@ class PhysicsObject {
     this.xSpeed = xSpeed; // Speed in the y direction. (Pixels per Frame ==> px/F)
     this.ySpeed = ySpeed; // Speed in the y direction. (Pixels per Frame ==> px/F)
     this.yAccel = 1.5; // Acceleration in the y direction (px/F²).
-    this.bounce = 0.25; // Amount of energy retained on collision (1 = 100%).
+    this.bounce = 0.35; // Amount of energy retained on collision (1 = 100%).
   }
 
   display() {
@@ -166,6 +172,8 @@ class PhysicsObject {
 
     if(this.shape==="rect") {
       rect(this.x,this.y,this.size);
+      fill(255);
+      text(round(this.ySpeed),this.x,this.y);
     } else if (this.shape === "circle") {
       circle(this.x,this.y,this.size);
     }
@@ -177,8 +185,8 @@ class PhysicsObject {
     }
   }
 
-  gravity() {
-    if(this.y+(this.size/2)+this.ySpeed < height) { // If object is above bottom of screen
+  gravity(bottomY) {
+    if(this.y+(this.size/2)+this.ySpeed < bottomY) { // If object is above bottom of screen
       this.y += this.ySpeed;
       this.ySpeed += this.yAccel;
     } else {
@@ -186,7 +194,7 @@ class PhysicsObject {
       // Resets y position and lowers speed to prevent oscillations.
       if(this.ySpeed < (this.yAccel*5) && this.ySpeed > (-this.yAccel*5)) {
         this.ySpeed -= (this.yAccel);
-        this.y = height-(this.size/2);
+        this.y = bottomY-(this.size/2);
       }
 
       // Inverts speed and applies bounce modifier to simulate a loss of energy.
@@ -214,9 +222,10 @@ class PhysicsObject {
     }
   }
 
-  isInBounds(x,y) {
+  isInBounds(x1,y1,x2,y2) {
     let radius = this.size/2;
-    if((x<this.x+radius)&&(x>this.x-radius)&&(y<this.y+radius)&&(y>this.y-radius)) {
+    let x3 = (x1 + x2)/2
+    if((x1<this.x+radius)&&(x1>this.x-radius)&&(y1<this.y+radius)&&(y1>this.y-radius) || (x2<this.x+radius)&&(x2>this.x-radius)&&(y2<this.y+radius)&&(y2>this.y-radius) || (x3<this.x+radius)&&(x3>this.x-radius)&&(y1<this.y+radius)&&(y1>this.y-radius)) {
       return true;
     } else { return false; }
   }
@@ -224,5 +233,16 @@ class PhysicsObject {
   getPos() {
     return([this.x,this.y]);
   }
+
+  collisions(i) {
+    for(let j = 0; j < physicsObjects.length; j++) {
+        let obj2 = physicsObjects[j];
+        if(obj2.isInBounds(this.x + this.size/2, this.y + this.size/2, this.x - this.size/2, this.y + this.size/2)) {
+          return(obj2.getPos()[1] - this.size/2 + 1);
+        }
+      }
+      return(height);
+  }
+
 }
 
