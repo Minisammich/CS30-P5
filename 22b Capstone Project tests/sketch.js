065ae2450acedf062381e8377b1ed84ab74a43f4
Minisammich <91817698+Minisammich@users.noreply.github.com>
2024-05-05 05:01:50 -0600

    aarrgghh

--- 22b Capstone Project tests/sketch.js
+++ 22b Capstone Project tests/sketch.js
@@ -11,9 +11,10 @@ let time = 0;
 let cubes = [];
 
 let cam;
-let sensitivity = 5;
+let sensitivity = 1;
 let mX = 0, mY = 0, dMX, dMY; // Mouse X/Y and Delta Mouse X/Y.
-let camLX = 0, camLY = 0, camLZ = 0; // Cam LookAt X/Y/Z.
+let camYaw = 0, camPitch = 0; // Camera yaw and pitch values.
+let camLX, camLY, camLZ; // Position the camera is looking at.
 let camX = 0, camY = -400, camZ = 800; // Cam X/Y/Z position.
 
 let currentCube = -1;
@@ -26,12 +27,13 @@ function setup() {
   //noCursor();
 
   cam = createCamera();
-  cam.setPosition(0,-400,800);
+  cam.setPosition(0,400,-800);
   cam.lookAt(0,0,0);
   cam.perspective(90);
 }
 
 function draw() {
+  sphere(0,0,0,500);
   background(220);
   getMousePositions();
   camMovement();
@@ -44,8 +46,9 @@ function draw() {
 }
 
 function mousePressed() {
+  //requestPointerLock();
   if(mouseButton === CENTER) {
-    cubes.push(new Cube(camLX,camLY,0, 0,0,0, 100));
+    cubes.push(new Cube(camLX,camLY+400,camLZ-800, 0,0,0, 100));
   }
 
   if(mouseButton === LEFT) {
@@ -84,14 +87,17 @@ function mouseWheel() {
 function getMousePositions() {
   dMY = mouseY - pmouseY;
   dMX = mouseX - pmouseX;
-  mX += dMX;
-  mY += dMY;
+  // mX += dMX;
+  // mY += dMY;
+  camYaw -= dMX/sensitivity;
+  camPitch += dMY/sensitivity;
 }
 
 function camMovement() {
-  camLX -= dMX/sensitivity; camLY += dMY/sensitivity;
-  // cam.pan(-dMX/sensitivity);
-  // cam.tilt(dMY/sensitivity);
+  camLX = cam.eyeX + cos(-camYaw) * cos(camPitch);
+  camLY = cam.eyeY + sin(camPitch);
+  camLZ = cam.eyeZ + sin(-camYaw) * cos(camPitch);
+  cam.lookAt(camLX,camLY,camLZ);
   cam.setPosition(camX,camY,camZ);
 }
 
@@ -106,7 +112,6 @@ class Cube {
 
   display() {
     push();
-    rotateX(camLX); rotateY(camLY); rotateZ(camLZ);
     translate(this.x,this.y,this.z);
     rotateX(this.rx); rotateY(this.ry); rotateZ(this.rz);
     fill(this.color);
