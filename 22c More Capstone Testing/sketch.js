bf9de99d7071c97882613c1771cda80c8af3d61f
Minisammich <91817698+Minisammich@users.noreply.github.com>
2024-05-21 14:35:49 -0600

    emotional distress cont.

--- 22c More Capstone Testing/sketch.js
+++ 22c More Capstone Testing/sketch.js
@@ -1,9 +1,6 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Capstone Platformer Testing
+// Jeffrey Hamilton
+// 18/May/2024
 
 let player;
 let walls = [];
@@ -17,86 +14,20 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
 
-  player = new Player(0,height/2);
-  walls.push(new Wall(width*0.5,height*0.2,500,100));
+  for(let i = 0; i < 10; i++) {
+    walls.push(new Wall(random(0,width),random(0,height),random(0,500),random(0,500)));
+  }
+
+  player = new Player(width/2,height/2);
 }
 
 function draw() {
   background(220);
 
-
   player.move();
   for(w of walls) {
-    w.display();
     w.collision();
+    w.display();
   }
   player.display();
 }
-
-class Player {
-  constructor(x,y) {
-    this.pos = createVector(x,y);
-    this.speed = 5;
-    this.size = createVector(50,80);
-  }
-
-  display() {
-    rect(this.pos.x,this.pos.y,this.size.x,this.size.y); // Will be a sprite at some point.
-  }
-
-  move() {
-    if(keyIsDown(188)) {
-      this.pos.y -= this.speed;
-    }
-    if(keyIsDown(79)) {
-      this.pos.y += this.speed;
-    }
-    if(keyIsDown(65)) {
-      this.pos.x -= this.speed;
-    }
-    if(keyIsDown(69)) {
-      this.pos.x += this.speed;
-    }
-  }
-
-  moveTo(x,y) {
-    this.pos.x = x, this.pos.y = y;
-  }
-
-  returnPos() {
-    return this.pos;
-  }
-
-  returnSize() {
-    return this.size;
-  }
-}
-
-class Wall {
-  constructor(x,y,w,h) {
-    this.x = x, this.y = y, this.w = w, this.h = h;
-  }
-
-  display() {
-    rect(this.x,this.y,this.w,this.h);
-  }
-
-  collision() {
-    let playerPos = player.returnPos();
-    let playerSize = player.returnSize();
-
-    let collideLeft = playerPos.x + playerSize.x/2 > this.x - this.w/2; 
-    let collideRight = playerPos.x - playerSize.x/2 < this.x + this.w/2;
-    let collideTop = playerPos.y + playerSize.y/2 > this.y - this.h/2;
-    let collideBottom = playerPos.y - playerSize.y/2 < this.y + this.h/2;
-
-    if(collideLeft && collideRight && collideBottom && collideTop) {
-      if(playerPos.x < this.x+this.w/2) {
-        player.moveTo((this.x-(this.w/2))-playerSize.x/2,playerPos.y);
-      } else {
-        player.moveTo((this.x+(this.w/2))+playerSize.x/2,playerPos.y);
-      }
-    }
-  }
-}
-
