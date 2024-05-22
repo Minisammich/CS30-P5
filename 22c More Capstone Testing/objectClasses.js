93109b6e9a38a31a644841b27086b14c50059f1f
Minisammich <91817698+Minisammich@users.noreply.github.com>
2024-05-22 15:20:43 -0600

    Wall Jump but No?

--- 22c More Capstone Testing/objectClasses.js
+++ 22c More Capstone Testing/objectClasses.js
@@ -1,3 +1,13 @@
+function keyPressed() {
+  if(keyCode === 188) {
+    if(player.canJump && player.maxJumpCount > 0) player.maxJumpCount--;
+
+    if(player.maxJumpCount <= 0 ) player.canJump = false;
+
+    if(player.canJump) player.speed.y = -15;
+  }
+}
+
 class Player {
     constructor(x,y) {
       this.pos = createVector(x,y);
@@ -9,20 +19,19 @@ class Player {
 
       this.acceleration = 0.5;
       this.size = createVector(50,80);
-      this.onFloor = false;
+      this.canJump = false;
+      this.maxJumpCount = 2;
     }
   
     display() {
       strokeWeight(2);
       fill(255,0,255);
       rect(this.pos.x,this.pos.y,this.size.x,this.size.y); // Will be a sprite at some point.
+      fill(255);
+      text(this.maxJumpCount,this.pos.x,this.pos.y);
     }
   
     move() {
-      if(keyIsDown(188)) {
-        if(this.onFloor) this.speed.y -= 15;
-        this.onFloor = false;
-      }
       if(keyIsDown(65) && !keyIsDown(69)) {
         if(this.speed.x > -this.maxSpeed) this.speed.x -= this.moveAccel;
       } else if(keyIsDown(69) && !keyIsDown(65)) {
@@ -34,27 +43,11 @@ class Player {
 
       this.pos.x += this.speed.x;
       this.gravity();
-      this.screenCollisions();
     }
   
     gravity() {
       this.speed.y += this.acceleration;
       this.pos.y += this.speed.y;
-    }
-  
-    screenCollisions() {
-      if(this.pos.x + this.size.x/2 > width) this.pos.x = width - this.size.x/2;
-      if(this.pos.x - this.size.x/2 < 0) this.pos.x = this.size.x/2;
-  
-      if(this.pos.y + this.size.y/2 > height) {
-        this.pos.y = height - this.size.y/2;
-        this.speed.y = 0;
-        this.onFloor = true;
-      }
-      if(this.pos.y - this.size.y/2 < 0) {
-        this.pos.y = this.size.y/2;
-        this.speed.y = 0;
-      }
     }
   
     moveTo(x,y) {
@@ -70,23 +63,26 @@ class Player {
     }
   }
   
-  class Wall {
-    constructor(x,y,w,h) {
+class Wall {
+    constructor(x,y,w,h,vis) {
       this.x = x, this.y = y, this.w = w, this.h = h;
+      this.vis = vis;
     }
   
     display() {
-      strokeWeight(1);
-      rect(this.x,this.y,this.w,this.h);
-      fill(255);
+      if(this.vis) {
+        strokeWeight(1);
+        rect(this.x,this.y,this.w,this.h);
+        fill(255);
+      }
     }
   
     collision() {
       let playerPos = player.returnPos();
       let playerSize = player.returnSize();
   
-      let collideLeft = playerPos.x + playerSize.x/2 > this.x - this.w/2; 
-      let collideRight = playerPos.x - playerSize.x/2 < this.x + this.w/2;
+      let collideLeft = playerPos.x + playerSize.x/2+1 > this.x - this.w/2; 
+      let collideRight = playerPos.x - playerSize.x/2-1 < this.x + this.w/2;
       let collideTop = playerPos.y + playerSize.y/2 > this.y - this.h/2;
       let collideBottom = playerPos.y - playerSize.y/2 < this.y + this.h/2;
   
@@ -97,23 +93,32 @@ class Player {
   
       if(collideTop && collideBottom && collideLeft && collideRight) {
         fill(0,255,0);
+
         if(distToBot < distToTop) {
+
           if(distToBot < distToRight && distToBot < distToLeft) {
             player.speed.y = 0;
             player.moveTo(playerPos.x,this.y+this.h/2+playerSize.y/2);
-          } else if(distToRight < distToLeft) {
-            player.speed.x = 0;
-            player.moveTo(this.x+this.w/2+playerSize.x/2,playerPos.y);
           } else {
-            player.speed.x = 0;
-            player.moveTo(this.x-this.w/2-playerSize.x/2,playerPos.y);
+            player.canJump = true;
+
+            if(distToRight < distToLeft) {
+              player.speed.x = 0;
+              player.moveTo(this.x+this.w/2+playerSize.x/2,playerPos.y);
+            } else {
+              player.speed.x = 0;
+              player.moveTo(this.x-this.w/2-playerSize.x/2,playerPos.y);
+            }
+
           }
+        } else if(distToTop < distToRight && distToTop < distToLeft) {
+          player.speed.y = 0;
+          player.canJump = true;
+          player.maxJumpCount = 2;
+          player.moveTo(playerPos.x,this.y-this.h/2-playerSize.y/2);
         } else {
-          if(distToTop < distToRight && distToTop < distToLeft) {
-            player.speed.y = 0;
-            player.onFloor = true;
-            player.moveTo(playerPos.x,this.y-this.h/2-playerSize.y/2);
-          } else if(distToRight < distToLeft) {
+          player.canJump = true;
+          if(distToRight < distToLeft) {
             player.speed.x = 0;
             player.moveTo(this.x+this.w/2+playerSize.x/2,playerPos.y);
           } else {
@@ -123,4 +128,5 @@ class Player {
         }
       }
     }
+
   }
--- 22c More Capstone Testing/sketch.js
+++ 22c More Capstone Testing/sketch.js
@@ -13,9 +13,16 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
+  textAlign(CENTER);
+
+  walls.push(new Wall(-25,height/2,50,height,true));
+  walls.push(new Wall(width+25,height/2,50,height,true));
+
+  walls.push(new Wall(width/2,-25,width,50,true));
+  walls.push(new Wall(width/2,height+25,width,50,true));
 
   for(let i = 0; i < 10; i++) {
-    walls.push(new Wall(random(0,width),random(0,height),random(0,500),random(0,500)));
+    walls.push(new Wall(random(0,width),random(0,height),random(0,500),random(0,500),true));
   }
 
   player = new Player(width/2,height/2);
@@ -26,9 +33,11 @@ function draw() {
   fill(255);
 
   player.move();
+
   for(w of walls) {
     w.collision();
     w.display();
   }
+
   player.display();
 }
